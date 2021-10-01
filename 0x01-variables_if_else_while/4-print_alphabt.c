8296b0bd5607c2b2f8eff946dbe5257927bc9299
alpagasauvage <3645@holbertonschool.com>
2021-10-01 13:10:04 +0000

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - lowercase
+ *Return: 0
+ */
+
+int main(void)
+{
+	 char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	if (letter == 'e' || letter == 'q')
+		{
+
+		}
+	else
+		{
+		putchar(letter);
+		}
+
+		putchar('\n');
+
+	return (0);
+}
