f306516931eab0d531e2bb6c7e0b10416c401947
alpagasauvage <3645@holbertonschool.com>
2021-10-01 20:20:20 +0000

    .

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - print comb5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+	int i = 0, j;
+
+	while (i <= 99)
+	{
+		j = i + 1;
+		a = i / 10;
+		b = i % 10;
+
+		while (j <= 99)
+		{
+			c = j / 10;
+			d = j % 10;
+
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(' ');
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (i <= 97 || j <= 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
