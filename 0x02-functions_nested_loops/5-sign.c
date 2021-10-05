4dbb1a6d05c328c7d337651e48abf6fcd162eb97
alpagasauvage <3645@holbertonschool.com>
2021-10-05 16:31:24 +0000

    .

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - print sign
+ *@n: value to check
+ *Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	return (0);
+}
