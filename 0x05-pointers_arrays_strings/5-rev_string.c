7f5ed324dd3f4851472ba7d51017273826286d60
alpagasauvage <3645@holbertonschool.com>
2021-10-11 11:53:13 +0000

    .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,38 @@
 #include "main.h"
 
+void rev_string(char *s);
+
+/**
+ *rev_string - reverse string
+ *@s: value
+ */
 
 void rev_string(char *s)
 {
-	
+
+	int length, start, temporary;
+
+	for (start = 0, length = _strlen(s) - 1; length > start; start++, length--)
+	{
+		temporary = s[start];
+		s[start] = s[length];
+		s[length] = temporary;
+	}
+}
+
+/**
+ *_strlen - lenght
+ *@s: value
+ *Return: i
+ */
+
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
