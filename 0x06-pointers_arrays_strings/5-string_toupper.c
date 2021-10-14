30ddb7e772af526bdc43a8cc23d27491761fee9e
AlpagaSauvage <3645@holbertonschool.com>
2021-10-14 11:01:00 +0000

    task 5 modified

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,13 @@ char *string_toupper(char *str)
 {
 	int c = 0;
 
-	while (str[c] >= 95 && str[c] <= 122)
+	while (str[c])
 	{
-		str[c] = str[c] - 32;
-		c++;
+		if (str[c] >= 95 && str[c] <= 122)
+		{
+			str[c] = str[c] - 32;
+		}
+	c++;
 	}
 	return (str);
 }
