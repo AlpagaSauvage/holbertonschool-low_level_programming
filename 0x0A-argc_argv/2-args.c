0cb0f32ed2bc11004b3f2531bcbb1434dd3ac178
AlpagaSauvage <3645@holbertonschool.com>
2021-10-22 12:57:01 +0000

    modified task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - print arguments
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	while (argc--)
 	{
 		printf("%s\n", *argv++);
 	}
-	return (0);	
+	return (0);
 }
