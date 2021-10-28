6b09e5f93fae6a1d7c8e529a70de7ab263389103
AlpagaSauvage <3645@holbertonschool.com>
2021-10-28 10:51:44 +0000

    add task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * malloc_checked - check if malloc work
+ * @b: size of malloc
+ * Return: c
+ */
 
 void *malloc_checked(unsigned int b)
 {
 	int *c;
 
-	c = malloc(sizeof(int) * b);
+	c = malloc(b);
 
 	if (c == NULL)
 	{
 		exit(98);
 	}
-	
+	return (c);
 }
