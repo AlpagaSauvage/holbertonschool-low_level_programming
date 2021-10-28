61509b2ecdb79a7073336afd820be37566ce23d2
AlpagaSauvage <3645@holbertonschool.com>
2021-10-28 13:14:25 +0000

    add task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - array of integrer
+ * @min: minimum
+ * @max: maximum
+ * Return: c
+ */
+
+int *array_range(int min, int max)
+{
+	int size, i;
+	int *c;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	for (size = 0; size < max - min; size++)
+	{
+	}
+
+	c = malloc(sizeof(int) * size + 1);
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size + 1; i++, min++)
+	{
+		c[i] = min;
+	}
+
+	return (c);
+}
