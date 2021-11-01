b333168c710892d6396c2a74672bb3887943edb5
AlpagaSauvage <3645@holbertonschool.com>
2021-11-01 19:44:41 +0000

    modified dog.h for task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+}dog;
+
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
