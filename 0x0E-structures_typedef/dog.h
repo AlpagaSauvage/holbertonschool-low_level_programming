ae2586b31c21090ca6456dd93bad22bdf610e069
AlpagaSauvage <3645@holbertonschool.com>
2021-11-01 18:22:01 +0000

    add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog_h
+#define dog_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - structure dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
