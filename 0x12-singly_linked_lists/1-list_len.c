98994bef91da8cc71be347e1e37a71cd80d7c640
AlpagaSauvage <3645@holbertonschool.com>
2021-11-12 12:28:27 +0000

    task 1 added

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+
+size_t list_len(const list_t *h)
+{
+	size_t element = 0;
+	const list_t *tmp_list;
+
+	if (h != NULL)
+	{
+		tmp_list = h;
+
+		while (tmp_list != NULL)
+		{
+			element++;
+			tmp_list = tmp_list->next;
+		}
+	}
+
+	return (element);
+}
