f64946cf2eb45d3ac824714d200e956b5bb24448
AlpagaSauvage <3645@holbertonschool.com>
2021-11-16 10:35:08 +0000

    add task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add node at the biginning
+ * @head: head of the node
+ * @n: value
+ * Return: head
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
+
+
+/**
+* _strlen - check the length of the string
+* Return: length of the string
+* @s: string
+*/
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
