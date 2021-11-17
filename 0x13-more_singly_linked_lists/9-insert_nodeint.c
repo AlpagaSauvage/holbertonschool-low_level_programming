138aedea4f17a4684a04d984cd6c00360c438c93
AlpagaSauvage <3645@holbertonschool.com>
2021-11-17 18:14:28 +0000

    modified task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (*head != NULL && c < idx)
+	while (tmp != NULL && c < idx)
 	{
 		previous = tmp;
 		tmp = tmp->next;
