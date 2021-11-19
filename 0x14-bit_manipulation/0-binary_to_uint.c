f0548f4ac3148188ae5ccaa97e177dba38224600
AlpagaSauvage <3645@holbertonschool.com>
2021-11-19 09:22:00 +0000

    add task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *_strlen - lenght
+ *@s: value
+ *Return: i
+ */
+
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
+
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: binary number
+ * Return: in decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int exposant = _strlen(b) - 1;
+	int i;
+	unsigned int result = 0, c = 1;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = exposant; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			result += c;
+		c *= 2;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int _putchar(char c);
+int _strlen(const char *s);
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
