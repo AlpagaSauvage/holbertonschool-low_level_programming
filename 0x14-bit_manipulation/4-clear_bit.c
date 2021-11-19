7ff3ca842ae7639a607bee377b56b57ab405ec3f
AlpagaSauvage <3645@holbertonschool.com>
2021-11-19 14:36:22 +0000

    add task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bit
+ * @n: value
+ * @index: index
+ * Return: 1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	*n &= ~(1 << index);
+
+
+	return (1);
+}
