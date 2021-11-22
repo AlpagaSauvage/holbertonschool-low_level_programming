e3853dc062d320fd8962751a2ce04e2698eb4c11
AlpagaSauvage <3645@holbertonschool.com>
2021-11-22 13:55:30 +0000

    add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - read file
+ * @filename: filemane
+ * @letters: letters
+ * Return: testread
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fo;
+	char *buffer = malloc(letters * (sizeof(char)));
+	int testread;
+
+	if (buffer == NULL)
+		return (0);
+
+	fo = open(filename, O_RDONLY);
+
+	if (fo == -1)
+		return (0);
+
+	if (*filename == -1)
+		return (0);
+
+	testread = read(fo, buffer, letters);
+
+	if (testread == -1)
+		return (0);
+
+	close(fo);
+
+	testread = write(1, buffer, testread);
+
+	if (testread == -1)
+		return (0);
+
+	free(buffer);
+
+	return (testread);
+}
