60a0c57c9d5e4171992e23a17ec5754e314b8d6c
AlpagaSauvage <3645@holbertonschool.com>
2021-11-22 16:27:01 +0000

    add task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,13 +18,13 @@ int _strlen(const char *s)
 }
 
 /**
- * create_file - create file if doen't exist
+ * append_text_to_file - add taxt to the end
  * @filename: filename
  * @text_content: text content
  * Return: 1
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fo;
 	int testwrite;
@@ -33,19 +33,24 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fo = open(filename, O_WRONLY | O_CREAT);
-
-	chmod(filename, 00600);
-
-	if (fo == -1)
-		return (-1);
-
-	if (text_content != NULL)
+	if (access(filename, F_OK) == 0)
 	{
+		fo = open(filename, O_WRONLY | O_APPEND);
+		if (fo == -1)
+			return (-1);
+
+		if (text_content == NULL)
+			return (1);
+
 		testwrite = write(fo, text_content, sizetext);
 
 		if (testwrite == -1)
 			return (-1);
+	}
+
+	else
+	{
+		return (-1);
 	}
 
 	close(fo);
