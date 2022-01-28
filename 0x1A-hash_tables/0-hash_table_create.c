6fbd368c8022e8529a71ddd6c70147a51f30a597
AlpagaSauvage <3645@holbertonschool.com>
2022-01-28 13:18:50 +0000

    modified task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,11 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_hash->size = size;
-	new_hash->array = malloc(sizeof(hash_node_t) * size);
+	new_hash->array = calloc(sizeof(hash_node_t) * size);
 	if (new_hash->array == NULL)
 	{
 		free(new_hash);
-		return(NULL);
+		return (NULL);
 	}
 
 	return (new_hash);
