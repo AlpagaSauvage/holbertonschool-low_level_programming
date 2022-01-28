a37e6ab3e54a70a5433131d87b1f2e812a427350
AlpagaSauvage <3645@holbertonschool.com>
2022-01-28 12:11:03 +0000

    betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash = malloc(sizeof(hash_table_t) * size);
 
 	if (new_hash == NULL)
-		return NULL
+		return (NULL);
 
 	return (new_hash);
 }
