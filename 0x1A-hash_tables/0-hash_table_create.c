7c8c9f21cce28ba9b1402876ab4526edead5d0ce
AlpagaSauvage <3645@holbertonschool.com>
2022-01-28 12:16:59 +0000

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,12 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash;
+	hash_table_t **new_hash;
 
 	new_hash = malloc(sizeof(hash_table_t) * size);
 
-	if (new_hash == NULL)
+	if (*new_hash == NULL)
 		return (NULL);
 
-	return (new_hash);
+	return (*new_hash);
 }
